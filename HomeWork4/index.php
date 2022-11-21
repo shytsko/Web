cd4b791ee54732cb0ca7d0542f095763bb1ed1a1
Vladimir Shytsko <shytsko.v@gmail.com>
2022-11-22 00:03:50 +0300

    Add task1

--- HomeWork4/index.php
+++ HomeWork4/index.php
@@ -22,6 +22,26 @@ $skills=[
                 "persent"=>100
             ]
        	];
+$experience = 	[
+					[
+                    	"name"=>"Фронтэнд разработчик",
+                        "description"=>"Разрабатывал фронтэнд",
+                        "date_from"=>strtotime("01.01.2015"),
+                        "date_to"=>time()
+                    ],
+                    [
+                    	"name"=>"Веб разработчик",
+                        "description"=>"Разрабатывал веб-сайты",
+                        "date_from"=>strtotime("01.03.2012"),
+                        "date_to"=>strtotime("31.12.2014")
+                    ],
+                    [
+                    	"name"=>"Дизайнер",
+                        "description"=>"Рисовал",
+                        "date_from"=>strtotime("01.06.2010"),
+                        "date_to"=>strtotime("28.02.2012")
+                    ]
+				];
 ?>
 <!DOCTYPE html>
 <html>
@@ -102,24 +122,19 @@ $skills=[
     
       <div class="w3-container w3-card w3-white w3-margin-bottom">
         <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Опыт работы</h2>
+        
+        <?php for($i=0; $i<count($experience); $i++) {?>
         <div class="w3-container">
-          <h5 class="w3-opacity"><b>Front End Developer </b></h5>
-          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Jan 2015 - <span class="w3-tag w3-teal w3-round">Current</span></h6>
-          <p>Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
+          <h5 class="w3-opacity"><b><?php echo $experience[$i]["name"]?></b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>
+          	<?php echo date('M Y', $experience[$i]["date_from"])?>
+            <?php echo " - "?>
+            <?php echo date('M Y', $experience[$i]["date_to"])?>
+          </h6>
+          <p><?php echo $experience[$i]["description"]?></p>
           <hr>
         </div>
-        <div class="w3-container">
-          <h5 class="w3-opacity"><b>Web Developer / something.com</b></h5>
-          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Mar 2012 - Dec 2014</h6>
-          <p>Consectetur adipisicing elit. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
-          <hr>
-        </div>
-        <div class="w3-container">
-          <h5 class="w3-opacity"><b>Graphic Designer / designsomething.com</b></h5>
-          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Jun 2010 - Mar 2012</h6>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p><br>
-        </div>
-      </div>
+        <?php }?>
 
       <div class="w3-container w3-card w3-white">
         <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Образование</h2>
